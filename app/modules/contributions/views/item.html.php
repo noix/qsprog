887908f9849d3c34369c685890649d9416e53d74
Etienne Despres <etienne.despres@molotovcoop.org>
2009-09-21 20:04:16 -0400

    Réglé l'affichage des commentaires généraux dans le module Contributions.

--- app/modules/contributions/views/item.html.php
+++ app/modules/contributions/views/item.html.php
@@ -2,21 +2,25 @@
 <p class="auteur">par <?= $cercle ?></p>
 <div class="sous-boite">
 <? if ($phase == 2): ?>
-<? switch ($typeModification):
-	case 1:
-		$typeString = 'est un amendement à';
-		break;
-	case 2:
-		$typeString = 's’inscrit immédiatement avant';
-		break;
-	case 3:
-		$typeString = 's’inscrit immédiatement après';
-		break;
-endswitch; ?>
-<p>Cette contribution <?= $typeString ?> la perspective <strong><?= $perspective_numero ?></strong>:</p>
-<div class="perspective">
-	<div class="texte"><?= TextRenderer::FormatText($perspective_texte) ?></div>
-</div>
+	<? if ($typeModification != 4): ?>
+	<? switch ($typeModification):
+		case 1:
+			$typeString = 'est un amendement à';
+			break;
+		case 2:
+			$typeString = 's’inscrit immédiatement avant';
+			break;
+		case 3:
+			$typeString = 's’inscrit immédiatement après';
+			break;
+	endswitch; ?>
+	<p>Cette contribution <?= $typeString ?> la perspective <strong><?= $perspective_numero ?></strong>:</p>
+	<div class="perspective">
+		<div class="texte"><?= TextRenderer::FormatText($perspective_texte) ?></div>
+	</div>
+	<? else: ?>
+	<p>Cette contribution est un commentaire général sur l’ensemble des perspectives.</p>
+	<? endif; ?>
 <? endif; ?>
 <?= $contribution ?>
 </div>
